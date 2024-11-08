ce20b2db810902f338f991105b8118615c59ff1c
Cheng Jingxuan <hbxycjx@outlook.com>
2024-11-08 21:18:32 +0800

    Experiment Page; Init NextAuth; Apply Styles

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,29 @@
+import NextAuth from "next-auth";
+import { uuidv7 as UuidV7 } from "uuidv7";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { authorize } from "@/app/backend/auth";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "Anonymous" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials) {
+        return credentials ? authorize(credentials.username, credentials.password) : null;
+      }
+    })
+  ],
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+    updateAge: 3 * 60 * 60,
+    async generateSessionToken() {
+      return UuidV7();
+    }
+  }
+});
+
+export { handler as GET, handler as POST };
