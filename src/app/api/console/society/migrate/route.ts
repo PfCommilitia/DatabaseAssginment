8d1b7b3c0acd0c23d1697b6a710969a76ff29f23
ylh1005 <2557642024@qq.com>
2024-12-05 09:20:49 +0800

    ylh_migrate_2

---
+++ src/app/api/console/society/migrate/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import { ERROR_UNKNOWN } from "@/app/dependencies/error/unknown";
+import ServerError from "@/app/dependencies/error/errorType";
+import { ERROR_USER_NOT_PERMITTED } from "@/app/dependencies/error/databaseTrigger";
+import migrateSociety from "@/app/dependencies/dataBackend/middleware/society/migrate";
+
+export async function POST(request: Request) {
+  try {
+    const { societyId, newOrganizationId } = await request.json();
+    // 调用API接口进行社团迁移
+    const result = await migrateSociety(societyId, newOrganizationId);
+    
+    if (!result) {
+      return NextResponse.json({ error: ERROR_USER_NOT_PERMITTED.code }, { status: 403 });
+    }
+
+    return NextResponse.json({ payload: {} }, { status: 200 });
+  } catch (e) {
+    if (!(e instanceof ServerError)) {
+      return NextResponse.json({ error: ERROR_UNKNOWN.code }, { status: 500 });
+    }
+    return NextResponse.json({ error: e.code }, { status: 500 });
+  }
+}
